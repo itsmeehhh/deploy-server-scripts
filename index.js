894d3f314df30bca2781e5d67cf38a283d1c4aab
itsmeehhh <101415734+itsmeehhh@users.noreply.github.com>
2024-06-01 21:36:30 +0000

    .

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ const app = express();
 const fetch = require('node-fetch');
 const path = require('path');
 
+const { exec } = require('child_process');
+
 const folderName = 'database';
 const children = {};
+
 function runFile(file) {
   const filePath = `./${folderName}/${file}`;
   const child = child_process.spawn('node', [filePath]);
@@ -14,20 +17,41 @@ function runFile(file) {
     console.log(data.toString());
   });
   child.stderr.on('data', (data) => {
- console.error(data.toString());
+    const message = data.toString();
+    console.error(message);
+    if (message.includes('Cannot find module')) {
+      const moduleName = message.match(/'([^']+)'/)[1];
+      stopFile(file);
+      installModule(moduleName, () => {
+        runFile(file);
+      });
+    }
   });
   child.on('close', (code) => {
     console.log(`child process exited with code ${code}`);
   });
   children[file] = child;
 }
+
 function stopFile(file) {
   const child = children[file];
   if (child) {
-    child.kill();
+    child.kill('SIGINT');
   }
   delete children[file];
 }
+
+function installModule(moduleName, callback) {
+  exec(`npm install ${moduleName}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error installing module ${moduleName}: ${stderr}`);
+      return;
+    }
+    console.log(`Module ${moduleName} installed successfully: ${stdout}`);
+    callback();
+  });
+}
+
 fs.readdir(folderName, (err, files) => {
   if (err) {
     console.error(err);
@@ -35,9 +59,10 @@ fs.readdir(folderName, (err, files) => {
   }
   files.forEach(runFile);
 });
+
 fs.watch(folderName, (eventType, filename) => {
   if (eventType === 'rename') {
-fs.access(`./${folderName}/${filename}`, (err) => {
+    fs.access(`./${folderName}/${filename}`, (err) => {
       if (err) {
         console.log(`File ${filename} was deleted`);
         stopFile(filename);
